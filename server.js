42d6741c9a0c1d8b68a6c6e23ef133f576f40eae
bree melechinsky <groundcontroltomajorbree@gmail.com>
2018-04-17 11:54:09 -0500

    add get requests

--- server.js
+++ server.js
@@ -3,4 +3,15 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-app.listen(PORT);
+//req is request, res is response
+app.get('/', function (req, res) {
+    res.send('hey world');
+});
+
+app.get('/hey', function (req, res) {
+    res.send('howdy');
+});
+
+app.listen(PORT, function () {
+    console.log(`listening on port: ${PORT}`);
+});
